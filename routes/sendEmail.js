141ea61d0753a3b01ccd7420c4277c46187e8391
vildashnetwork <vildashnetwork02@gmail.com>
2026-03-30 06:46:44 +0100

    height of the image

--- routes/sendEmail.js
+++ routes/sendEmail.js
@@ -3,22 +3,25 @@ import axios from "axios";
 export const sendBrevoEmail = async (options) => {
     try {
         const apiKey = process.env.BREVO_API_KEY;
+        const supportEmail = process.env.SUPPORT_EMAIL;
+
         const url = "https://api.brevo.com/v3/smtp/email";
 
         const emailContent = {
             sender: {
-                name: 'TimberTrade',
-                email: process.env.SUPPORT_EMAIL
+                name: 'Your Project Admin', // Change as needed
+                email: supportEmail
             },
             to: [
                 {
-                    email: options.email || "vildashnetwork02@gmail.com",
-                    name: options.name || "User"
+                    email: options.email || "liblissz3@gmail.com",
+                    name: options.name || "Admin"
                 }
             ],
             subject: options.subject,
             htmlContent: options.html,
-            textContent: options.text || ''
+            // ADD THIS LINE BELOW - It solves the 'missing_parameter' error
+            textContent: options.text || "New notification from your website."
         };
 
         const response = await axios.post(url, emailContent, {
